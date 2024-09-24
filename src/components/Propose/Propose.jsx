fed2ea2cca135d882b893056f5a626429db64b12
JasurAvazov <jasuravazov4@gmail.com>
2024-09-24 20:53:56 +0500

    fix

--- src/components/Propose/Propose.jsx
+++ src/components/Propose/Propose.jsx
@@ -10,22 +10,22 @@ const Propose = () => {
                 <p className={styles.title}>Мы предлагаем</p>
                 <div className={styles.grid}>
                     <div className={`${styles.item} ${styles.big}`}>
-                        <p className={styles.itemTitle}>Партнерство</p>
+                        <p className={styles.itemTitle}>Передовые решение</p>
                         <img src="/propose-1.png" alt="" draggable={false} />
                     </div>
                     <div className={`${styles.item} ${styles.big}`}>
-                        <p className={styles.itemTitle}>Поддержку на протяжение курса</p>
+                        <p className={styles.itemTitle}>Поддержка вашего бизнеса</p>
                         <img src="/propose-2.png" alt="" draggable={false} />
                     </div>
                     <div className={styles.col}>
                         <div className={styles.item}>
-                            <p className={styles.itemTitle}>IT заказы</p>
-                            <a href="#!" className={styles.itemBtn}>Детали заказа</a>
+                            <p className={styles.itemTitle}>Партнерство</p>
+                            <a href="#!" className={styles.itemBtn}>Детали</a>
                             <img src="/propose-3.png" alt="" draggable={false} />
                         </div>
                         <div className={styles.item}>
-                            <p className={styles.itemTitle}>IT Вакансии</p>
-                            <a href="#!" className={styles.itemBtn}>Вакансии</a>
+                            <p className={styles.itemTitle}>Маштаб</p>
+                            <a href="#!" className={styles.itemBtn}>Детали</a>
                             <img src="/propose-4.png" alt="" draggable={false} />
                         </div>
                     </div>
