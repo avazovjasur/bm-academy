4d7c0b8706bdc6b3fd8a81f2a0c1475a4bf34d9e
JasurAvazov <avazovjasur95@gmail.com>
2024-10-10 18:11:25 +0500

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,9 +12,11 @@ const Header = ({ setPopup }) => {
   const [showMenu, setShowMenu] = useState(false);
   const { locale } = router
   const t = locale === 'uz' ? uz : locale === 'en' ? en : ru
+  const [showLang, setShowLang] = useState(false)
 
   const changeLanguage = (e) => {
-    const locale = e
+    const locale = e;
+    setShowLang(false)
     router.push(router.pathname, router.asPath, { locale })
   }
 
@@ -24,6 +26,19 @@ const Header = ({ setPopup }) => {
         <button type="button" className={styles.back} onClick={() => { setShowMenu(false) }}>
           <img src="back.svg" alt="" />
         </button>
+        <div className={styles.dropDown}>
+            <button onClick={() => setShowLang(!showLang)} type="button" className={`${styles.dropDownLabel} ${styles.dropDownLang}`}>
+              {locale === 'ru' ? 'RU' : locale === 'en' ? 'EN' : 'UZ' }
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.46485 8.02915C5.16534 7.77242 4.71441 7.80711 4.45768 8.10663C4.20095 8.40614 4.23563 8.85707 4.53515 9.1138L9.53515 13.3995C9.80265 13.6288 10.1974 13.6288 10.4649 13.3995L15.4649 9.1138C15.7644 8.85707 15.7991 8.40614 15.5423 8.10663C15.2856 7.80711 14.8347 7.77242 14.5352 8.02915L10 11.9164L5.46485 8.02915Z" fill="#152040" />
+              </svg>
+            </button>
+            <div className={`${styles.dropDownInner} ${showLang ? styles.active : ''}`}>
+              <button type="button" onClick={e => changeLanguage("ru")} className={`${styles.dropDownBtn} ${locale === 'ru' ? styles.active : ''}`}>RU</button>
+              <button type="button" onClick={e => changeLanguage("uz")} className={`${styles.dropDownBtn} ${locale === 'uz' ? styles.active : ''}`}>UZ</button>
+              <button type="button" onClick={e => changeLanguage("en")} className={`${styles.dropDownBtn} ${locale === 'en' ? styles.active : ''}`}>EN</button>
+            </div>
+          </div>
         <a href="#about" onClick={() => { setShowMenu(false) }} className={styles.menuBtn}>
           {t['about_us']}
         </a>
@@ -59,13 +74,13 @@ const Header = ({ setPopup }) => {
         </div>
         <div className={styles.right}>
           <div className={styles.dropDown}>
-            <button type="button" className={`${styles.dropDownLabel} ${styles.dropDownLang}`}>
+            <button onClick={() => setShowLang(!showLang)} type="button" className={`${styles.dropDownLabel} ${styles.dropDownLang}`}>
               {locale === 'ru' ? 'RU' : locale === 'en' ? 'EN' : 'UZ' }
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.46485 8.02915C5.16534 7.77242 4.71441 7.80711 4.45768 8.10663C4.20095 8.40614 4.23563 8.85707 4.53515 9.1138L9.53515 13.3995C9.80265 13.6288 10.1974 13.6288 10.4649 13.3995L15.4649 9.1138C15.7644 8.85707 15.7991 8.40614 15.5423 8.10663C15.2856 7.80711 14.8347 7.77242 14.5352 8.02915L10 11.9164L5.46485 8.02915Z" fill="#152040" />
               </svg>
             </button>
-            <div className={styles.dropDownInner}>
+            <div className={`${styles.dropDownInner} ${showLang ? styles.active : ''}`}>
               <button type="button" onClick={e => changeLanguage("ru")} className={`${styles.dropDownBtn} ${locale === 'ru' ? styles.active : ''}`}>RU</button>
               <button type="button" onClick={e => changeLanguage("uz")} className={`${styles.dropDownBtn} ${locale === 'uz' ? styles.active : ''}`}>UZ</button>
               <button type="button" onClick={e => changeLanguage("en")} className={`${styles.dropDownBtn} ${locale === 'en' ? styles.active : ''}`}>EN</button>
